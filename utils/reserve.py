ae727802a8ee9d37a8cce315249226b6d1b563db
summit <83833208+Summit-Teoh@users.noreply.github.com>
2024-09-04 21:20:43 +0800

    Update reserve.py
    更改reserve部分，以配合main的更改

--- utils/reserve.py
+++ utils/reserve.py
@@ -6,11 +6,15 @@ import time
 import logging
 import datetime
 from urllib3.exceptions import InsecureRequestWarning
+
+#这个函数用于获取当前日期，或者根据 day_offset 获取相对于今天的某个日期。
+# 返回格式为 YYYY-MM-DD
 def get_date(day_offset: int=0):
     today = datetime.datetime.now().date()
     offset_day = today + datetime.timedelta(days=day_offset)
     tomorrow = offset_day.strftime("%Y-%m-%d")
     return tomorrow
+
 
 class reserve:
     def __init__(self, sleep_time=0.2, max_attempt=50, enable_slider=False, reserve_next_day=False):
@@ -80,7 +84,7 @@ class reserve:
         jsons = self.requests.post(
             url=self.login_url, params=parm, verify=False)
         obj = jsons.json()
-        if obj['status']:
+        if obj['status']:#status状态可为true或false
             logging.info(f"User {username} login successfully")
             return (True, '')
         else:
@@ -96,7 +100,7 @@ class reserve:
             info = f'{i["firstLevelName"]}-{i["secondLevelName"]}-{i["thirdLevelName"]} id为：{i["id"]}'
             print(info)
 
-    # solve captcha 
+    # solve captcha 处理验证码部分
 
     def resolve_captcha(self):
         logging.info(f"Start to resolve captcha token")
@@ -196,7 +200,7 @@ class reserve:
         tl = max_loc
         return tl[0]
 
-     #submit 方法负责执行预约流程。它在尝试预约之前会获取页面 token，
+ #submit 方法负责执行预约流程。它在尝试预约之前会获取页面 token，
  # 并根据需要解决验证码。然后使用 get_submit 提交预约请求。新的的时间段预约要求新的submit请求（每次请求的token不同）
     def submit(self, times, roomid, seatid, action):
         for seat in seatid:
@@ -235,3 +239,66 @@ class reserve:
             time_slot[0] + "~" + time_slot[1] + ':  ' + str(json.loads(html)))
         logging.info(json.loads(html))
         return json.loads(html)["success"]
+    
+
+    def sign_in(self):
+        day = datetime.date.today().strftime("%Y-%m-%d")
+        sign_data = self.get_my_seat_id(day)    #sign_date得到是前面得到的json字典（当日预约记录）
+        need_sign_data = []
+        for index in sign_data:
+            if index['status'] == 1:
+                print("[exist]今日已签: {}".format(
+                    index['firstLevelName'] + index['secondLevelName'] + index['thirdLevelName'] + index['seatNum']))
+            if index['status'] == 0 or index['status'] == 3 or index['status'] == 5:
+                need_sign_data.append(index)
+                continue
+        if need_sign_data:
+            for need_sign in need_sign_data:
+                response = self.requests.get(
+                    url='https://office.chaoxing.com/data/apps/seat/sign?id={}'.format(need_sign["id"]))
+                location = need_sign['firstLevelName'] + need_sign['secondLevelName'] + need_sign['thirdLevelName'] + \
+                           need_sign['seatNum']
+                if response.json()['success']:
+                    print("[true]成功签到: {}".format(location))
+                else:
+                    print("[false]签到失败: {}\n反馈信息: {}".format(location, response.json()['msg']))
+        else:
+            print("[none]今日没有需要签到的数据")
+    
+
+    def get_my_seat_id(self, current_date):
+        response = self.requests.get(url='https://office.chaoxing.com/data/apps/seat/reservelist?'
+                                         'indexId=0&'
+                                         'pageSize=100&'
+                                         'type=-1').json()['data']['reserveList']   #这里的url是查询座位记录，此处的type用于查询预约类型
+        result = []
+        for index in response:
+            if index['type'] == -1:                         #type值为‘-1’表示未违约
+                if index['today'] == current_date:
+                    # 今日签到信息
+                    result.append(index)           #传这个字典的核心目的是为了拿到里面的id，签到要携带此字段
+        return result                              #id是从预约记录里面拿的，即签到不需要桌位号（拓展性想法：这个ID是本地生成的还是客户端传过来的）
+    
+ #----------------------------------------------------------------------------------------------------------
+    #不同的type值代表不同的违约类型 ，status表示不同的状态
+        
+    #  status:          0 '待履约'                              type:      1 : 使用完毕未退座 
+    #                   1:'学习中'                                         0 : 未在规定时间内签到
+    #                   2:'已履约'                                         5 : 被监督后，未在规定时间内落座
+    #                   3:'暂离中'                                         3 : 暂离后未在规定时间内返回
+    #                   5:'被监督中'
+    #                   7:'已取消'
+ #-----------------------------------------------------------------------------------------------------------
+    def signback_(self):
+        day = datetime.date.today().strftime("%Y-%m-%d")
+        info = self.get_my_seat_id(day)
+        for index in info:
+            if index['status'] == 1 or index['status'] == 3 or index['status'] == 5:
+                location = index['firstLevelName'] + index['secondLevelName'] + index['thirdLevelName'] + index[
+                    'seatNum']
+                response = self.requests.get(
+                    url='https://office.chaoxing.com/data/apps/seat/signback?id={}'.format(index['id']))
+                if response.json()['success']:
+                    return "{}：座位已退出".format(location)
+                return "{}：{}".format(location, response.json()['msg'])
+        return "当前没有座位可退"
